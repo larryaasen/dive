432212cbe000abd332710d1a9c66aee08f1a518e
Larry Aasen <larryaasen@gmail.com>
2023-03-11 14:33:59 -0500

    Updated many of the package dependency versions including Riverpod, uuid, ffi, ffigen, and path_provider.

--- packages/dive/lib/src/dive_uuid.dart
+++ packages/dive/lib/src/dive_uuid.dart
@@ -1,7 +1,8 @@
 import 'package:uuid/uuid.dart';
 
 /// Simple, fast generation of RFC4122 UUIDs
-final _uuid = Uuid();
+// ignore: prefer_const_constructors
+var _uuid = Uuid();
 
 abstract class DiveUuid {
   static String newId() => _uuid.v1();
